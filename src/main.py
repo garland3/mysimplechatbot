a1ea2bb77cafa936946fd5438d08d1967e8ad0bb
anthony garland <garland3@gmail.com>
2023-03-10 18:12:04 -0700

    working now

--- src/main.py
+++ src/main.py
@@ -9,7 +9,7 @@ from database import get_single_db, UserSession, ChatMessage
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-
+db = get_single_db() 
 
 @app.post("/chatbot")
 async def chatbot(request: Request):
@@ -23,10 +23,10 @@ async def chatbot(request: Request):
    # Log chat message to database
     timestamp = datetime.now()
     user_session_id = data.get('user_session_id')
-    with get_single_db() as db:
-        db.add(ChatMessage(user_session_id=user_session_id, message=question, timestamp=timestamp))
-        db.add(ChatMessage(user_session_id=user_session_id, message=response, timestamp=timestamp))
-        db.commit()
+   
+    db.add(ChatMessage(user_session_id=user_session_id, message=question, timestamp=timestamp))
+    db.add(ChatMessage(user_session_id=user_session_id, message=response, timestamp=timestamp))
+    db.commit()
 
     return {"response": response}
 
@@ -41,10 +41,9 @@ async def index():
     ip = '0.0.0.0'  # replace with actual user IP
     username = 'greg'  # replace with actual authenticated user
     chat_id = 1  # replace with actual chat ID
-    with get_single_db() as db:
-        user_session = UserSession(ip=ip, username=username, chat_id=chat_id, start_time=timestamp)
-        db.add(user_session)
-        db.commit()
+    user_session = UserSession(ip=ip, username=username, chat_id=chat_id, start_time=timestamp)
+    db.add(user_session)
+    db.commit()
 
     # Inject user session ID into HTML
     html = html.replace('{{ user_session_id }}', str(user_session.id))
