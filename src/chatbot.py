30401fec277112046b551ba074e0dfb7978bbd5b
anthony garland <garland3@gmail.com>
2023-04-16 23:47:52 -0600

    made it read a config for the url

--- src/chatbot.py
+++ src/chatbot.py
@@ -1,19 +1,11 @@
-# from transformers import pipeline, set_seed
-
-# generator = pipeline("text-generation", model="EleutherAI/gpt-neo-125M")
-# set_seed(42)
-
-# def generate_response(question):
-#     print("Generating response for question: ", question)
-#     response = generator(question, max_length=250, do_sample=True, temperature=0.7)[0]["generated_text"]
-#     response = response.split(question)[1].strip()
-#     return response
-
 
 import requests
 import json
+from config import settings
 
-url = "http://localhost:8002/run"
+url = settings.backendurl
+
+# url = "http://localhost:8002/run"
 headers = {
     "accept": "application/json",
     "api-key": "xx1asafs9uaxlkn",
@@ -26,6 +18,22 @@ def generate_response(question):
     response = requests.post(url, headers=headers, data=json.dumps(data))
     # return response of the json
     return response.json()['response']
+
+
+
+
+
+# from transformers import pipeline, set_seed
+
+# generator = pipeline("text-generation", model="EleutherAI/gpt-neo-125M")
+# set_seed(42)
+
+# def generate_response(question):
+#     print("Generating response for question: ", question)
+#     response = generator(question, max_length=250, do_sample=True, temperature=0.7)[0]["generated_text"]
+#     response = response.split(question)[1].strip()
+#     return response
+
 
 
 # import requests
